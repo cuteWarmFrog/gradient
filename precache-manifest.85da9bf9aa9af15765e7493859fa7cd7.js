6c48d9e3f0a3a3cbd8f044a89eca10ca71aef130
Victor Livshits <livshits.victor@gmail.com>
2020-05-17 17:49:19 +0300

    Updates

---
+++ precache-manifest.85da9bf9aa9af15765e7493859fa7cd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02024daee14acf56fe13bda7ee5a4bd9",
+    "url": "/gradient/index.html"
+  },
+  {
+    "revision": "fc35bcc1e4c4db73c2b8",
+    "url": "/gradient/static/css/main.e4c627ac.chunk.css"
+  },
+  {
+    "revision": "d01188e0216c88cd3081",
+    "url": "/gradient/static/js/2.b3b1a3e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gradient/static/js/2.b3b1a3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc35bcc1e4c4db73c2b8",
+    "url": "/gradient/static/js/main.bd736e64.chunk.js"
+  },
+  {
+    "revision": "c3294b4cf2a7f1c7d1b3",
+    "url": "/gradient/static/js/runtime-main.53bfa6d6.js"
+  }
+]);
